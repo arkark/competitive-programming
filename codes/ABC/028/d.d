49bef0ef3458d3ceb9788d3cc1cb39b96b221eb6
Ark <target.ark@gmail.com>
2016-12-03 21:03:09 +0900

    ABC 028

---
+++ codes/ABC/028/d.d
@@ -0,0 +1,37 @@
+import std.stdio;
+import std.string;
+import std.conv;
+import std.typecons;
+import std.algorithm;
+import std.functional;
+import std.bigint;
+import std.numeric;
+import std.array;
+import std.math;
+import std.range;
+import std.container;
+import std.ascii;
+void times(alias fun)(int n) {
+    foreach(i; 0..n) fun();
+}
+auto rep(alias fun, T = typeof(fun()))(int n) {
+    T[] res = new T[n];
+    foreach(ref e; res) e = fun();
+    return res;
+}
+// fold was added in D 2.071.0.
+template fold(fun...) if (fun.length >= 1) {
+    auto fold(R, S...)(R r, S seed) {
+        static if (S.length < 2) {
+            return reduce!fun(seed, r);
+        } else {
+            return reduce!fun(tuple(seed), r);
+        }
+    }
+}
+
+void main() {
+    long N, K;
+    readf("%d %d\n", &N, &K);
+    writefln("%0.10f", ((N-K)*(K-1)*6 + (N-1)*3 + 1)/cast(double)N^^3);
+}
