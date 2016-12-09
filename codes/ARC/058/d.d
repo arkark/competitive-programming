765ecfe0bc78c874e0d25bda3d7968a744c35761
Ark <target.ark@gmail.com>
2016-12-09 11:53:37 +0900

    ARC 058 C,D

--- codes/ARC/058/d.d
+++ codes/ARC/058/d.d
@@ -1,3 +1,54 @@
+import std.stdio;
+import std.string;
+import std.conv;
+import std.typecons;
+import std.algorithm;
+import std.functional;
+import std.bigint;
+import std.numeric;
+import std.array;
+import std.math;
+import std.range;
+import std.container;
+import std.ascii;
+void times(alias fun)(int n) {
+    foreach(i; 0..n) fun();
+}
+auto rep(alias fun, T = typeof(fun()))(int n) {
+    T[] res = new T[n];
+    foreach(ref e; res) e = fun();
+    return res;
+}
+// fold was added in D 2.071.0.
+template fold(fun...) if (fun.length >= 1) {
+    auto fold(R, S...)(R r, S seed) {
+        static if (S.length < 2) {
+            return reduce!fun(seed, r);
+        } else {
+            return reduce!fun(tuple(seed), r);
+        }
+    }
+}
+
+immutable long MOD = 10^^9+7;
+alias ModLong = ModNum!(long, MOD);
+void main() {
+    int H, W, A, B;
+    readf("%d %d %d %d\n", &H, &W, &A, &B);
+
+    ModLong[] fact = new ModLong[H+W+1];
+    ModLong[] _fact = new ModLong[H+W+1];
+    foreach(i, ref e; fact) {
+        e = i==0 ? ModLong(1) : ModLong(i) * fact[i-1];
+    }
+    foreach(i; _fact.length.iota.retro) {
+        _fact[i] = i==H+W ? ModLong(1)/fact[i] : ModLong(i+1) * _fact[i+1];
+    }
+    iota(B, W).map!(i =>
+        fact[H-A-1+i]*_fact[i]*_fact[H-A-1] * fact[W+A-2-i]*_fact[A-1]*_fact[W-1-i]
+    ).sum.writeln;
+}
+
 struct ModNum(T, T mod) if (__traits(isIntegral, T)) {
     T value;
     typeof(this) opBinary(string op)(typeof(this) that) if (op=="+" || op=="-" || op=="*") {
