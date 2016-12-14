1fff1f2631f331671ea7c93285378ec48ce65ce9
Ark <target.ark@gmail.com>
2016-12-14 23:44:00 +0900

    AGC001e

--- codes/AGC/001/e.d
+++ codes/AGC/001/e.d
@@ -11,15 +11,44 @@ import std.math;
 import std.range;
 import std.container;
 import std.ascii;
-void times(int n, void delegate() pred) {
-    foreach(i; 0..n) pred();
+import std.concurrency;
+void times(alias fun)(int n) {
+    foreach(i; 0..n) fun();
 }
-T[] rep(T)(int n, T delegate() pred) {
+auto rep(alias fun, T = typeof(fun()))(int n) {
     T[] res = new T[n];
-    foreach(ref e; res) e = pred();
+    foreach(ref e; res) e = fun();
     return res;
+}
+// fold was added in D 2.071.0.
+template fold(fun...) if (fun.length >= 1) {
+    auto fold(R, S...)(R r, S seed) {
+        static if (S.length < 2) {
+            return reduce!fun(seed, r);
+        } else {
+            return reduce!fun(tuple(seed), r);
+        }
+    }
 }
 
 void main() {
-    
+    int N, M;
+    readf("%d %d\n", &N, &M);
+    int[] ary = readln.split.to!(int[]).sort!((a, b) =>
+        (a+b)%2==0 ? a<b : a%2==1
+    ).array.pipe!"a[1..$]~a[0]";
+    if (M==1) ary ~= 0;
+
+    if (ary.count!"a%2==1" > 2) {
+        writeln("Impossible");
+        return;
+    }
+
+    ary.filter!"a>0".array.to!(string[]).join(" ").writeln;
+    ary.front--;
+    ary.back++;
+    ary.filter!"a>0".array.pipe!((a) {
+        a.length.writeln;
+        a.to!(string[]).join(" ").writeln;
+    });
 }
