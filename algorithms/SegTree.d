629d6257c9a32afa1944189d70751c470ff1704b
Ark <target.ark@gmail.com>
2017-06-04 04:34:47 +0900

    SegTreeの更新

--- algorithms/SegTree.d
+++ algorithms/SegTree.d
@@ -2,8 +2,8 @@
 alias RMQ(T) = SegTree!(T, (a, b) => a<b ? a:b, T.max);
 
 // SegTree (Segment Tree)
-struct SegTree(T, alias pred, T initValue)
-    if (is(typeof(pred(T.init, T.init)) : T)) {
+struct SegTree(T, alias fun, T initValue)
+    if (is(typeof(fun(T.init, T.init)) : T)) {
 
 private:
     T[] _data;
@@ -23,6 +23,7 @@ public:
         update(ary);
     }
 
+    // O(N)
     void init(size_t size){
         _size = 1;
         while(_size < size) {
@@ -35,16 +36,18 @@ public:
     }
 
     // i番目の要素をxに変更
+    // O(logN)
     void update(size_t i, T x) {
         i += _size-1;
         _data[i] = x;
         while(i>0) {
             i = (i-1)/2;
-            _data[i] = pred(_data[i*2+1], _data[i*2+2]);
+            _data[i] = fun(_data[i*2+1], _data[i*2+2]);
         }
     }
 
     // 配列で指定
+    // o(N)
     void update(T[] ary) {
         foreach(size_t i, e; ary) {
             size_t _i = i+_size-1;
@@ -52,22 +55,24 @@ public:
         }
         foreach(i; 0.._size-1) {
             size_t _i = _size-i-2;
-            _data[_i] = pred(_data[_i*2+1], _data[_i*2+2]);
+            _data[_i] = fun(_data[_i*2+1], _data[_i*2+2]);
         }
     }
 
     // 区間[a, b)でのクエリ
+    // O(logN)
     T query(size_t a, size_t b) {
-        return _query(a, b, 0, 0, _size);
-    }
-    T _query(size_t a, size_t b, size_t k, size_t l, size_t r) {
-        if (b<=l || r<=a) return initValue;
-        if (a<=l && r<=b) return _data[k];
-        T vl = _query(a, b, k*2+1, l, (l+r)/2);
-        T vr = _query(a, b, k*2+2, (l+r)/2, r);
-        return pred(vl, vr);
+        T rec(size_t a, size_t b, size_t k, size_t l, size_t r) {
+            if (b<=l || r<=a) return initValue;
+            if (a<=l && r<=b) return _data[k];
+            T vl = rec(a, b, k*2+1, l, (l+r)/2);
+            T vr = rec(a, b, k*2+2, (l+r)/2, r);
+            return fun(vl, vr);
+        }
+        return rec(a, b, 0, 0, _size);
     }
 
+    // O(N)
     T[] array() {
         return _data[_l+_size-1.._r+_size-1];
     }
