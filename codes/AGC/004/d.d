eecf1b46691c9d76fc622c3eb647a2ba155023e3
Ark <target.ark@gmail.com>
2017-07-07 21:25:01 +0900

    AGC004 d

--- codes/AGC/004/d.d
+++ codes/AGC/004/d.d
@@ -11,40 +11,222 @@ import std.math;
 import std.range;
 import std.container;
 import std.ascii;
-void times(alias pred)(int n) {
-    foreach(i; 0..n) pred();
+import std.concurrency;
+void times(alias fun)(int n) {
+    // n.iota.each!(i => fun());
+    foreach(i; 0..n) fun();
 }
-auto rep(alias pred, T = typeof(pred()))(int n) {
+auto rep(alias fun, T = typeof(fun()))(int n) {
+    // return n.iota.map!(i => fun()).array;
     T[] res = new T[n];
-    foreach(ref e; res) e = pred();
+    foreach(ref e; res) e = fun();
     return res;
 }
 
+class Vertex{
+    Vertex[] from;
+}
 void main() {
     int N, K;
     readf("%d %d\n", &N, &K);
-    int[] ary = readln.split.to!(int[]).map!"a-1".array;
+    int[] as = readln.split.to!(int[]).map!"a-1".array;
 
-    int[][] edges = new int[][N];
-    ary.enumerate.drop(1).each!(a => edges[a.value] ~= a.index.to!int);
+    int ans = 0;
 
-    int[] dist = new int[N];
-    dist.front = 0;
-    N.rep!(()=>false).pipe!((flg) {
-        flg.front = true;
-        auto list = DList!int(0);
-        while(!list.empty) {
-            int i = list.front;
-            list.removeFront;
-            edges[i].each!((j) {
-                if (!flg[j]) {
-                    list.insertBack(j);
-                    flg[j] = true;
-                    dist[j] = dist[i]+1;
-                }
-            });
+    Vertex[] vs = N.rep!(() => new Vertex);
+    foreach(i, a; as) {
+        if (i==0) {
+            if (a!=0) ans++;
+        } else {
+            vs[a].from ~= vs[i];
         }
-    });
+    }
 
-    ((ary.front!=0 ? 1:0) + dist.drop(1).count!(a => a>1 && (a-1)%K==0)).writeln;
+    int rec(Vertex v) {
+        int t = v.from.map!(u => rec(u)).map!(
+            (a) {
+                if (a==K && v!=vs.front) {
+                    ans++;
+                    return 0;
+                }
+                return a;
+            }
+        ).fold!max(0);
+        return t+1;
+    }
+    rec(vs.front);
+
+    ans.writeln;
+}
+
+// ----------------------------------------------
+
+// fold was added in D 2.071.0
+static if (__VERSION__ < 2071) {
+    template fold(fun...) if (fun.length >= 1) {
+        auto fold(R, S...)(R r, S seed) {
+            static if (S.length < 2) {
+                return reduce!fun(seed, r);
+            } else {
+                return reduce!fun(tuple(seed), r);
+            }
+        }
+    }
+}
+
+// cumulativeFold was added in D 2.072.0
+static if (__VERSION__ < 2072) {
+    template cumulativeFold(fun...)
+    if (fun.length >= 1)
+    {
+        import std.meta : staticMap;
+        private alias binfuns = staticMap!(binaryFun, fun);
+
+        auto cumulativeFold(R)(R range)
+        if (isInputRange!(Unqual!R))
+        {
+            return cumulativeFoldImpl(range);
+        }
+
+        auto cumulativeFold(R, S)(R range, S seed)
+        if (isInputRange!(Unqual!R))
+        {
+            static if (fun.length == 1)
+                return cumulativeFoldImpl(range, seed);
+            else
+                return cumulativeFoldImpl(range, seed.expand);
+        }
+
+        private auto cumulativeFoldImpl(R, Args...)(R range, ref Args args)
+        {
+            import std.algorithm.internal : algoFormat;
+
+            static assert(Args.length == 0 || Args.length == fun.length,
+                algoFormat("Seed %s does not have the correct amount of fields (should be %s)",
+                    Args.stringof, fun.length));
+
+            static if (args.length)
+                alias State = staticMap!(Unqual, Args);
+            else
+                alias State = staticMap!(ReduceSeedType!(ElementType!R), binfuns);
+
+            foreach (i, f; binfuns)
+            {
+                static assert(!__traits(compiles, f(args[i], e)) || __traits(compiles,
+                        { args[i] = f(args[i], e); }()),
+                    algoFormat("Incompatible function/seed/element: %s/%s/%s",
+                        fullyQualifiedName!f, Args[i].stringof, E.stringof));
+            }
+
+            static struct Result
+            {
+            private:
+                R source;
+                State state;
+
+                this(R range, ref Args args)
+                {
+                    source = range;
+                    if (source.empty)
+                        return;
+
+                    foreach (i, f; binfuns)
+                    {
+                        static if (args.length)
+                            state[i] = f(args[i], source.front);
+                        else
+                            state[i] = source.front;
+                    }
+                }
+
+            public:
+                @property bool empty()
+                {
+                    return source.empty;
+                }
+
+                @property auto front()
+                {
+                    assert(!empty, "Attempting to fetch the front of an empty cumulativeFold.");
+                    static if (fun.length > 1)
+                    {
+                        import std.typecons : tuple;
+                        return tuple(state);
+                    }
+                    else
+                    {
+                        return state[0];
+                    }
+                }
+
+                void popFront()
+                {
+                    assert(!empty, "Attempting to popFront an empty cumulativeFold.");
+                    source.popFront;
+
+                    if (source.empty)
+                        return;
+
+                    foreach (i, f; binfuns)
+                        state[i] = f(state[i], source.front);
+                }
+
+                static if (isForwardRange!R)
+                {
+                    @property auto save()
+                    {
+                        auto result = this;
+                        result.source = source.save;
+                        return result;
+                    }
+                }
+
+                static if (hasLength!R)
+                {
+                    @property size_t length()
+                    {
+                        return source.length;
+                    }
+                }
+            }
+
+            return Result(range, args);
+        }
+    }
+}
+
+// minElement/maxElement was added in D 2.072.0
+static if (__VERSION__ < 2072) {
+    auto minElement(alias map, Range)(Range r)
+    if (isInputRange!Range && !isInfinite!Range)
+    {
+        alias mapFun = unaryFun!map;
+        auto element = r.front;
+        auto minimum = mapFun(element);
+        r.popFront;
+        foreach(a; r) {
+            auto b = mapFun(a);
+            if (b < minimum) {
+                element = a;
+                minimum = b;
+            }
+        }
+        return element;
+    }
+    auto maxElement(alias map, Range)(Range r)
+    if (isInputRange!Range && !isInfinite!Range)
+    {
+        alias mapFun = unaryFun!map;
+        auto element = r.front;
+        auto maximum = mapFun(element);
+        r.popFront;
+        foreach(a; r) {
+            auto b = mapFun(a);
+            if (b > maximum) {
+                element = a;
+                maximum = b;
+            }
+        }
+        return element;
+    }
 }
