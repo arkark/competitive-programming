9252e5d927bcec1c3536e444a72d99e7a3a158e1
Ark <target.ark@gmail.com>
2017-08-17 21:55:02 +0900

    Update Prim.d

--- algorithms/Prim.d
+++ algorithms/Prim.d
@@ -1,59 +1,53 @@
 // プリム法
 // O(E+VlogV)
 // 無向グラフの最小全域木問題
-struct Prim(T) {
-    import std.algorithm;
-    import std.container; // rbtree
+struct Prim(T, T inf = T.max) {
+
 private:
     Vertex[] _vertices;
 
 public:
-    this(size_t size, T inf = T.max) {
+    this(size_t size) {
         _vertices.length = size;
         foreach(i; 0..size) {
-            _vertices[i] = new Vertex(i, inf);
+            _vertices[i] = new Vertex(i);
         }
     }
 
-    void setEdge(size_t start, size_t end, T weight) {
+    void addEdge(size_t start, size_t end, T weight) {
         _vertices[start].edges ~= Edge(start, end, weight);
-        _vertices[end].edges ~= Edge(end, start, weight);
-    }
-
-    void setEdge(T[] input) {
-        setEdge(cast(size_t) input[0], cast(size_t) input[1], input[2]);
+        _vertices[end].edges   ~= Edge(end, start, weight);
     }
 
     T solve() {
         _vertices.front.cost = 0;
-        auto rbtree = redBlackTree!"a.cost==b.cost ? a.index<b.index : a.cost<b.cost"(_vertices.front);
-        while(!rbtree.empty) {
-            Vertex v = rbtree.front;
-            v.flag = true;
-            rbtree.removeFront;
+        auto tree = redBlackTree!(
+            "a.cost==b.cost ? a.index<b.index : a.cost<b.cost",
+            true,
+            Vertex
+        )(_vertices.front);
+        while(!tree.empty) {
+            Vertex v = tree.front;
+            v.used = true;
+            tree.removeFront;
             v.edges.each!((Edge e) {
-                if (_vertices[e.end].flag) return;
-                rbtree.removeKey(_vertices[e.end]);
+                if (_vertices[e.end].used) return;
+                tree.removeKey(_vertices[e.end]);
                 _vertices[e.end].cost = min(_vertices[e.end].cost, e.weight);
-                rbtree.insert(_vertices[e.end]);
+                tree.insert(_vertices[e.end]);
             });
         }
-        return reduce!"a + b.cost"(0, _vertices);
-    }
-
-    Vertex[] getVertices() {
-        return _vertices;
+        return _vertices.map!"a.cost".sum;
     }
 
 private:
     class Vertex {
         size_t index;
-        bool flag;
-        T cost;
-        Edge[] edges = [];
-        this(size_t index, T cost) {
+        bool used;
+        T cost = inf;
+        Edge[] edges;
+        this(size_t index) {
             this.index = index;
-            this.cost = cost;
         }
         override string toString() {return "";} //
     }
