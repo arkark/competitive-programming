76f20c7c2e019709147d76670b938eb616f41616
Ark <target.ark@gmail.com>
2018-01-13 22:33:12 +0900

    Solve dwacon2018 prelims A, B, C, E(400)

---
+++ codes/others/dwacon2018/prelims/e.d
@@ -0,0 +1,286 @@
+import std.stdio;
+import std.string;
+import std.conv;
+import std.typecons;
+import std.algorithm;
+import std.functional;
+import std.bigint;
+import std.numeric;
+import std.array;
+import std.math;
+import std.range;
+import std.container;
+import std.ascii;
+import std.concurrency;
+import std.format;
+import core.bitop : popcnt;
+alias Generator = std.concurrency.Generator;
+
+const long INF = long.max/3;
+const long MOD = 10L^^9+7;
+
+void main() {
+    int N, Q;
+    scanln(N, Q);
+
+    Vertex[] vs = N.iota.map!(i => new Vertex(i)).array;
+    foreach(_; 0..N-1) {
+        int a, b;
+        scanln(a, b);
+        a--; b--;
+        vs[a].edges ~= Edge(vs[a], vs[b]);
+        vs[b].edges ~= Edge(vs[b], vs[a]);
+
+        // !!!!
+        if (vs[a].edges.length > 2) return;
+        if (vs[b].edges.length > 2) return;
+        // !!!!
+    }
+
+    Vertex[] us = new Vertex[N];
+    us[0] = vs.find!"a.edges.length == 1".front;
+    foreach(i; 1..N) {
+        if (i==1) {
+            us[i] = us[i-1].edges[0].end;
+        } else {
+            us[i] = us[i-1].edges.map!"a.end".find!(u => u !is us[i-2]).front;
+        }
+    }
+    int l = 0;
+    int r = N;
+    while(r-l>1) {
+        int ans = query(us[l].index, us[r-1].index);
+        if (ans == us[l].index) {
+            r = (l + r)/2;
+        } else {
+            l = (l + r)/2;
+        }
+    }
+    "! %d".format(us[l].index + 1).writeln;
+}
+
+class Vertex {
+    int index;
+    Edge[] edges;
+    this(int index) {
+        this.index = index;
+    }
+}
+
+struct Edge {
+    Vertex start, end;
+}
+
+
+
+int query(int u, int v) {
+    "? %d %d".format(u+1, v+1).writeln;
+    stdout.flush;
+    int ans;
+    scanln(ans);
+    return ans<0 ? u : ans - 1;
+}
+
+// ----------------------------------------------
+
+void scanln(Args...)(ref Args args) {
+    foreach(i, ref v; args) {
+        "%d".readf(&v);
+        (i==args.length-1 ? "\n" : " ").readf;
+    }
+    // ("%d".repeat(args.length).join(" ") ~ "\n").readf(args);
+}
+
+void times(alias fun)(int n) {
+    // n.iota.each!(i => fun());
+    foreach(i; 0..n) fun();
+}
+auto rep(alias fun, T = typeof(fun()))(int n) {
+    // return n.iota.map!(i => fun()).array;
+    T[] res = new T[n];
+    foreach(ref e; res) e = fun();
+    return res;
+}
+
+T ceil(T)(T x, T y) if (__traits(isIntegral, T)) {
+    // `(x+y-1)/y` will only work for positive numbers ...
+    T t = x / y;
+    if (t * y < x) t++;
+    return t;
+}
+
+T floor(T)(T x, T y) if (__traits(isIntegral, T)) {
+    T t = x / y;
+    if (t * y > x) t--;
+    return t;
+}
+
+// fold was added in D 2.071.0
+static if (__VERSION__ < 2071) {
+    template fold(fun...) if (fun.length >= 1) {
+        auto fold(R, S...)(R r, S seed) {
+            static if (S.length < 2) {
+                return reduce!fun(seed, r);
+            } else {
+                return reduce!fun(tuple(seed), r);
+            }
+        }
+    }
+}
+
+// cumulativeFold was added in D 2.072.0
+static if (__VERSION__ < 2072) {
+    template cumulativeFold(fun...)
+    if (fun.length >= 1)
+    {
+        import std.meta : staticMap;
+        private alias binfuns = staticMap!(binaryFun, fun);
+
+        auto cumulativeFold(R)(R range)
+        if (isInputRange!(Unqual!R))
+        {
+            return cumulativeFoldImpl(range);
+        }
+
+        auto cumulativeFold(R, S)(R range, S seed)
+        if (isInputRange!(Unqual!R))
+        {
+            static if (fun.length == 1)
+                return cumulativeFoldImpl(range, seed);
+            else
+                return cumulativeFoldImpl(range, seed.expand);
+        }
+
+        private auto cumulativeFoldImpl(R, Args...)(R range, ref Args args)
+        {
+            import std.algorithm.internal : algoFormat;
+
+            static assert(Args.length == 0 || Args.length == fun.length,
+                algoFormat("Seed %s does not have the correct amount of fields (should be %s)",
+                    Args.stringof, fun.length));
+
+            static if (args.length)
+                alias State = staticMap!(Unqual, Args);
+            else
+                alias State = staticMap!(ReduceSeedType!(ElementType!R), binfuns);
+
+            foreach (i, f; binfuns)
+            {
+                static assert(!__traits(compiles, f(args[i], e)) || __traits(compiles,
+                        { args[i] = f(args[i], e); }()),
+                    algoFormat("Incompatible function/seed/element: %s/%s/%s",
+                        fullyQualifiedName!f, Args[i].stringof, E.stringof));
+            }
+
+            static struct Result
+            {
+            private:
+                R source;
+                State state;
+
+                this(R range, ref Args args)
+                {
+                    source = range;
+                    if (source.empty)
+                        return;
+
+                    foreach (i, f; binfuns)
+                    {
+                        static if (args.length)
+                            state[i] = f(args[i], source.front);
+                        else
+                            state[i] = source.front;
+                    }
+                }
+
+            public:
+                @property bool empty()
+                {
+                    return source.empty;
+                }
+
+                @property auto front()
+                {
+                    assert(!empty, "Attempting to fetch the front of an empty cumulativeFold.");
+                    static if (fun.length > 1)
+                    {
+                        import std.typecons : tuple;
+                        return tuple(state);
+                    }
+                    else
+                    {
+                        return state[0];
+                    }
+                }
+
+                void popFront()
+                {
+                    assert(!empty, "Attempting to popFront an empty cumulativeFold.");
+                    source.popFront;
+
+                    if (source.empty)
+                        return;
+
+                    foreach (i, f; binfuns)
+                        state[i] = f(state[i], source.front);
+                }
+
+                static if (isForwardRange!R)
+                {
+                    @property auto save()
+                    {
+                        auto result = this;
+                        result.source = source.save;
+                        return result;
+                    }
+                }
+
+                static if (hasLength!R)
+                {
+                    @property size_t length()
+                    {
+                        return source.length;
+                    }
+                }
+            }
+
+            return Result(range, args);
+        }
+    }
+}
+
+// minElement/maxElement was added in D 2.072.0
+static if (__VERSION__ < 2072) {
+    auto minElement(alias map, Range)(Range r)
+    if (isInputRange!Range && !isInfinite!Range)
+    {
+        alias mapFun = unaryFun!map;
+        auto element = r.front;
+        auto minimum = mapFun(element);
+        r.popFront;
+        foreach(a; r) {
+            auto b = mapFun(a);
+            if (b < minimum) {
+                element = a;
+                minimum = b;
+            }
+        }
+        return element;
+    }
+    auto maxElement(alias map, Range)(Range r)
+    if (isInputRange!Range && !isInfinite!Range)
+    {
+        alias mapFun = unaryFun!map;
+        auto element = r.front;
+        auto maximum = mapFun(element);
+        r.popFront;
+        foreach(a; r) {
+            auto b = mapFun(a);
+            if (b > maximum) {
+                element = a;
+                maximum = b;
+            }
+        }
+        return element;
+    }
+}
---
+++ codes/others/dwacon2018/prelims/po.md
@@ -0,0 +1,3 @@
+# 第4回 ドワンゴからの挑戦状 予選
+
+https://beta.atcoder.jp/contests/dwacon2018-prelims/standings
