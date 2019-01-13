50560ce8bba3d0d44ec526f5e4f948a8edb6b14c
Ark <target.ark@gmail.com>
2019-01-14 08:33:27 +0900

    Add QuickFind

---
+++ lib/QuickFind.d
@@ -0,0 +1,53 @@
+struct QuickFind {
+
+private:
+  size_t[] i2g;
+  size_t[][] gs;
+
+public:
+  this(size_t size) {
+    init(size);
+  }
+
+  void init(size_t size) {
+    gs.length = size;
+    i2g.length = size;
+    foreach(i; 0..size) {
+      gs[i] = [i];
+      i2g[i] = i;
+    }
+  }
+
+  void unite(size_t x, size_t y) {
+    if (gs[i2g[x]].length < gs[i2g[y]].length) {
+      swap(x, y);
+    }
+    size_t gx = i2g[x], gy = i2g[y];
+    if (gx == gy) return;
+    foreach(i; gs[gy]) {
+      i2g[i] = gx;
+    }
+    gs[gx] ~= gs[gy];
+    gs[gy].length = 0;
+  }
+
+  bool same(size_t x, size_t y) {
+    return findSet(x) == findSet(y);
+  }
+
+  size_t findSet(size_t index) {
+    return i2g[index];
+  }
+
+  bool isRoot(size_t index) {
+    return findSet(index) == index;
+  }
+
+  size_t[] getSet(size_t index) {
+    return gs[findSet(index)];
+  }
+
+  size_t[][] getAllSet() {
+    return gs;
+  }
+}
