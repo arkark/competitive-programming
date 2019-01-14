4051b4f1fb172b6930a22b49471fa34c50a6d23d
Ark <target.ark@gmail.com>
2019-01-15 01:02:05 +0900

    Solve keyence2019 E

--- src/AtCoder/others/keyence2019/e1.d
+++ src/AtCoder/others/keyence2019/e1.d
@@ -136,16 +136,16 @@ private:
   }
 }
 
-
 // RMQ (Range Minimum Query)
 alias RMQ(T) = SegTree!(T, (a, b) => a<b ? a:b, T.max);
 
 // SegTree (Segment Tree)
-struct SegTree(T, alias fun, T initValue)
+//    - with 1-based array
+struct SegTree(T, alias fun, T initValue, bool structly = true)
   if (is(typeof(fun(T.init, T.init)) : T)) {
 
 private:
-  Node[] _data;
+  Pair[] _data;
   size_t _size;
   size_t _l, _r;
 
@@ -168,8 +168,8 @@ public:
     while(_size < size) {
       _size *= 2;
     }
-    _data.length = _size*2-1;
-    _data[] = Node(size_t.max, initValue);
+    _data.length = _size*2;
+    _data[] = Pair(size_t.max, initValue);
     _l = 0;
     _r = size;
   }
@@ -178,12 +178,11 @@ public:
   // O(logN)
   void update(size_t i, T x) {
     size_t index = i;
-    i += _size-1;
-    _data[i] = Node(index, x);
+    _data[i += _size] = Pair(index, x);
     while(i > 0) {
-      i = (i-1)/2;
-      Node nl = _data[i*2+1];
-      Node nr = _data[i*2+2];
+      i >>= 1;
+      Pair nl = _data[i*2+0];
+      Pair nr = _data[i*2+1];
       _data[i] = select(nl, nr);
     }
   }
@@ -192,61 +191,87 @@ public:
   // O(N)
   void update(T[] ary) {
     foreach(i, e; ary) {
-      _data[i+_size-1] = Node(i, e);
+      _data[i+_size] = Pair(i, e);
     }
-    foreach(i; (_size-1).iota.retro) {
-      Node nl = _data[i*2+1];
-      Node nr = _data[i*2+2];
+    foreach_reverse(i; 1.._size) {
+      Pair nl = _data[i*2+0];
+      Pair nr = _data[i*2+1];
       _data[i] = select(nl, nr);
     }
   }
 
-  // 区間[a, b)でのクエリ (値の取得)
+  // 区間[a, b)でのクエリ (valueの取得)
   // O(logN)
   T query(size_t a, size_t b) {
-    return queryRec(a, b, 0, 0, _size).value;
+    Pair pair = accumulate(a, b);
+    // Pair pair = queryRec(a, b, 0, 0, _size);
+    return pair.value;
   }
 
   // 区間[a, b)でのクエリ (indexの取得)
   // O(logN)
   size_t queryIndex(size_t a, size_t b) out(result) {
     // fun == (a, b) => a+b のようなときはindexを聞くとassertion
-    assert(result != size_t.max);
+    if (structly) assert(result != size_t.max);
   } body {
-    return queryRec(a, b, 0, 0, _size).index;
+    Pair pair = accumulate(a, b);
+    // Pair pair = queryRec(a, b, 0, 0, _size);
+    return pair.index;
   }
 
-  private Node queryRec(size_t a, size_t b, size_t k, size_t l, size_t r) {
-    if (b<=l || r<=a) return Node(size_t.max, initValue);
-    if (a<=l && r<=b) return _data[k];
-    Node nl = queryRec(a, b, k*2+1, l, (l+r)/2);
-    Node nr = queryRec(a, b, k*2+2, (l+r)/2, r);
-    return select(nl, nr);
+  // 区間[a, b)でのクエリ ((index, value)の取得)
+  // O(logN)
+  Pair queryPair(size_t a, size_t b) out(result) {
+    // fun == (a, b) => a+b のようなときはindexを聞くとassertion
+    if (structly) assert(result.index != size_t.max);
+  } body {
+    Pair pair = accumulate(a, b);
+    // Pair pair = queryRec(a, b, 0, 0, _size);
+    return pair;
   }
 
-  private Node select(Node nl, Node nr) {
+  private Pair accumulate(size_t l, size_t r) {
+    if (r<=_l || _r<=l) return Pair(size_t.max, initValue);
+    Pair accl = Pair(size_t.max, initValue);
+    Pair accr = Pair(size_t.max, initValue);
+    for (l += _size, r += _size; l < r; l >>= 1, r >>= 1) {
+      if (l&1) accl = select(accl, _data[l++]);
+      if (r&1) accr = select(accr, _data[r-1]);
+    }
+    return select(accl, accr);
+  }
+
+  // private Pair queryRec(size_t a, size_t b, size_t k, size_t l, size_t r) {
+  //   if (b<=l || r<=a) return Pair(size_t.max, initValue);
+  //   if (a<=l && r<=b) return _data[k];
+  //   size_t c = (l+r)/2;
+  //   Pair nl = queryRec(a, b, k*2+0, l, c);
+  //   Pair nr = queryRec(a, b, k*2+1, c, r);
+  //   return select(nl, nr);
+  // }
+
+  private Pair select(Pair nl, Pair nr) {
     T v = fun(nl.value, nr.value);
     if (nl.value == v) {
       return nl;
     } else if (nr.value == v) {
       return nr;
     } else {
-      return Node(size_t.max, v);
+      return Pair(size_t.max, v);
     }
   }
 
   // O(1)
   T get(size_t i) {
-    return _data[_size-1 + i].value;
+    return _data[_size + i].value;
   }
 
   // O(N)
   T[] array() {
-    return _data[_l+_size-1.._r+_size-1].map!"a.value".array;
+    return _data[_l+_size.._r+_size].map!"a.value".array;
   }
 
-private:
-  struct Node {
+  struct Pair {
     size_t index;
     T value;
   }
