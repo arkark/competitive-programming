79c06e9c528dd2079e61273a619c02265da856b1
Takeshi Kaneko <target.ark@gmail.com>
2019-11-27 18:15:52 +0900

    Update ABC040

--- src/AtCoder/ABC/031_040/040/c.d
+++ src/AtCoder/ABC/031_040/040/c.d
@@ -1,5 +1,6 @@
 import std.stdio;
 import std.string;
+import std.format;
 import std.conv;
 import std.typecons;
 import std.algorithm;
@@ -10,27 +11,121 @@ import std.array;
 import std.math;
 import std.range;
 import std.container;
-import std.ascii;
+import std.concurrency;
+import std.traits;
+import std.uni;
+import core.bitop : popcnt;
+alias Generator = std.concurrency.Generator;
 
-void times(int n, void delegate() pred) {
-    foreach(i; 0..n) pred();
-}
-T[] rep(T)(int n, T delegate() pred) {
-    T[] res = new T[n];
-    foreach(ref e; res) e = pred();
-    return res;
-}
+enum long INF = long.max/5;
+enum long MOD = 10L^^9+7;
 
-long INF = 1L<<60;
 void main() {
-    int N = readln.chomp.to!int;
-    long[] a = readln.split.to!(long[]);
+    long N;
+    scanln(N);
+    long[] as = readln.split.to!(long[]);
+
     long[] dp = new long[N];
-    dp[1..$] = INF;
+    dp[] = INF;
+    dp[0] = 0;
+
     foreach(i; 0..N) {
-        foreach(j; 0..2) {
-            if (i>j) dp[i] = min(dp[i], dp[i-j-1]+abs(a[i]-a[i-j-1]));
-        }
+        if (i+1 < N) dp[i+1].ch!min(dp[i] + abs(as[i+1] - as[i]));
+        if (i+2 < N) dp[i+2].ch!min(dp[i] + abs(as[i+2] - as[i]));
     }
     dp.back.writeln;
+}
+
+// ----------------------------------------------
+
+
+void times(alias fun)(long n) {
+  // n.iota.each!(i => fun());
+  foreach(i; 0..n) fun();
+}
+auto rep(alias fun, T = typeof(fun()))(long n) {
+  // return n.iota.map!(i => fun()).array;
+  T[] res = new T[n];
+  foreach(ref e; res) e = fun();
+  return res;
+}
+
+T ceil(T)(T x, T y) if (isIntegral!T || is(T == BigInt)) {
+  // `(x+y-1)/y` will only work for positive numbers ...
+  T t = x / y;
+  if (t * y < x) t++;
+  return t;
+}
+
+T floor(T)(T x, T y) if (isIntegral!T || is(T == BigInt)) {
+  T t = x / y;
+  if (t * y > x) t--;
+  return t;
+}
+
+ref T ch(alias fun, T, S...)(ref T lhs, S rhs) {
+  return lhs = fun(lhs, rhs);
+}
+unittest {
+  long x = 1000;
+  x.ch!min(2000);
+  assert(x == 1000);
+  x.ch!min(3, 2, 1);
+  assert(x == 1);
+  x.ch!max(100).ch!min(1000); // clamp
+  assert(x == 100);
+  x.ch!max(0).ch!min(10); // clamp
+  assert(x == 10);
+}
+
+mixin template Constructor() {
+  import std.traits : FieldNameTuple;
+  this(Args...)(Args args) {
+    // static foreach(i, v; args) {
+    foreach(i, v; args) {
+      mixin("this." ~ FieldNameTuple!(typeof(this))[i]) = v;
+    }
+  }
+}
+
+void scanln(Args...)(auto ref Args args) {
+  enum sep = " ";
+  enum n = Args.length;
+  enum fmt = n.rep!(()=>"%s").join(sep) ~ "\n";
+  static if (__VERSION__ >= 2071) {
+    readf!fmt(args);
+  } else {
+    enum argsTemp = n.iota.map!(
+      i => "&args[%d]".format(i)
+    ).join(", ");
+    mixin(
+      "readf(fmt, " ~ argsTemp ~ ");"
+    );
+  }
+}
+
+// fold was added in D 2.071.0
+static if (__VERSION__ < 2071) {
+  template fold(fun...) if (fun.length >= 1) {
+    auto fold(R, S...)(R r, S seed) {
+      static if (S.length < 2) {
+        return reduce!fun(seed, r);
+      } else {
+        return reduce!fun(tuple(seed), r);
+      }
+    }
+  }
+}
+
+// popcnt with ulongs was added in D 2.071.0
+static if (__VERSION__ < 2071) {
+  ulong popcnt(ulong x) {
+    x = (x & 0x5555555555555555L) + (x>> 1 & 0x5555555555555555L);
+    x = (x & 0x3333333333333333L) + (x>> 2 & 0x3333333333333333L);
+    x = (x & 0x0f0f0f0f0f0f0f0fL) + (x>> 4 & 0x0f0f0f0f0f0f0f0fL);
+    x = (x & 0x00ff00ff00ff00ffL) + (x>> 8 & 0x00ff00ff00ff00ffL);
+    x = (x & 0x0000ffff0000ffffL) + (x>>16 & 0x0000ffff0000ffffL);
+    x = (x & 0x00000000ffffffffL) + (x>>32 & 0x00000000ffffffffL);
+    return x;
+  }
 }
