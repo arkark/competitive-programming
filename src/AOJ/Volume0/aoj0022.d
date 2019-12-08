a44b94c1e2facfd0e6225b5e07e845b6fcd0bd9b
Takeshi Kaneko <target.ark@gmail.com>
2019-12-08 23:18:19 +0900

    Update AOJ 0022

--- src/AOJ/Volume0/aoj0022.d
+++ src/AOJ/Volume0/aoj0022.d
@@ -1,26 +1,45 @@
-import std.stdio, std.string, std.algorithm, std.conv;
-const long INF = long.max/3;
+import std.stdio;
+import std.string;
+import std.algorithm;
+import std.functional;
+import std.conv;
+
+enum long INF = long.max/5;
 
 void main() {
-    while(solve()){}
+  while(solve()){}
 }
 
 bool solve() {
-    int N = readln.chomp.to!int;
-    if (N == 0) return false;
+  int n = readln.chomp.to!int;
+  if (n == 0) return false;
 
-    long[] as = new long[N];
-    foreach(i; 0..N) {
-        as[i] = readln.chomp.to!long;
-    }
+  long[] as = new long[n];
+  foreach(i; 0..n) {
+    as[i] = readln.chomp.to!long;
+  }
 
-    long res = -INF;
-    long s = 0;
-    foreach(i; 0..N) {
-        s = max(as[i], s + as[i]);
-        res = max(res, s);
-    }
-    res.writeln;
+  kadane!(
+    long, max, -INF, "a+b", 0
+  )(as).writeln;
 
-    return true;
+  return true;
+}
+
+T kadane(
+  T,              // 型
+  alias plusFun,  // 加法の２項演算
+  T plusIdentity, // 加法の単位元
+  alias multFun,  // 乗法の２項演算
+  T multIdentity  // 乗法の単位元
+)(T[] as) {
+  alias _plusFun = binaryFun!plusFun;
+  alias _multFun = binaryFun!multFun;
+  T res = plusIdentity;
+  T s = multIdentity;
+  foreach(a; as) {
+    s = _plusFun(_multFun(s, a), a);
+    res = _plusFun(res, s);
+  }
+  return res;
 }
