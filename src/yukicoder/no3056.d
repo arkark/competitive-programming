62222ab4b81f72930132fed48e41bb97afbcc662
Takeshi Kaneko <target.ark@gmail.com>
2020-02-06 03:19:51 +0900

    Solve yukicoder No.3056

---
+++ src/yukicoder/no3056.d
@@ -0,0 +1,160 @@
+module yukicoder.no3056;
+
+import std.stdio;
+import std.string;
+import std.format;
+import std.conv;
+import std.typecons;
+import std.algorithm;
+import std.functional;
+import std.bigint;
+import std.numeric;
+import std.array;
+import std.math;
+import std.range;
+import std.container;
+import std.concurrency;
+import std.traits;
+import std.uni;
+import core.bitop : popcnt;
+alias Generator = std.concurrency.Generator;
+import std.random;
+
+void main() {
+  auto rnd = Random(unpredictableSeed);
+
+  BigInt n = readln.chomp.to!BigInt;
+  while(true) {
+    BigInt a = uniform!("[]", ulong)(1, min(n-1, ulong.max.to!BigInt).to!ulong, rnd);
+    if (gcd(a, n) != 1) continue;
+    BigInt r = query(a, n);
+    if (r&1) continue;
+    BigInt b = a.powmod(r>>1, n);
+    BigInt p = gcd(b+1, n);
+    BigInt q = gcd(b-1, n);
+    if (p == n || q == n) continue;
+    answer(p, q);
+    break;
+  }
+}
+
+T query(T)(T a, T n) {
+  writeln("? ", a);
+  stdout.flush;
+  debug {
+    T r = 0;
+    T b = 1;
+    while(true) {
+      b = b*a%n;
+      r++;
+      if (b == 1) {
+        return r;
+      }
+    }
+    assert(false);
+  } else {
+    return readln.chomp.to!T;
+  }
+}
+
+void answer(T)(T p, T q) {
+  writeln("! ", p, " ", q);
+}
+
+// ----------------------------------------------
+
+
+void times(alias fun)(long n) {
+  // n.iota.each!(i => fun());
+  foreach(i; 0..n) fun();
+}
+auto rep(alias fun, T = typeof(fun()))(long n) {
+  // return n.iota.map!(i => fun()).array;
+  T[] res = new T[n];
+  foreach(ref e; res) e = fun();
+  return res;
+}
+
+T ceil(T)(T x, T y) if (isIntegral!T || is(T == BigInt)) {
+  // `(x+y-1)/y` will only work for positive numbers ...
+  T t = x / y;
+  if (y > 0 && t * y < x) t++;
+  if (y < 0 && t * y > x) t++;
+  return t;
+}
+
+T floor(T)(T x, T y) if (isIntegral!T || is(T == BigInt)) {
+  T t = x / y;
+  if (y > 0 && t * y > x) t--;
+  if (y < 0 && t * y < x) t--;
+  return t;
+}
+
+ref T ch(alias fun, T, S...)(ref T lhs, S rhs) {
+  return lhs = fun(lhs, rhs);
+}
+unittest {
+  long x = 1000;
+  x.ch!min(2000);
+  assert(x == 1000);
+  x.ch!min(3, 2, 1);
+  assert(x == 1);
+  x.ch!max(100).ch!min(1000); // clamp
+  assert(x == 100);
+  x.ch!max(0).ch!min(10); // clamp
+  assert(x == 10);
+}
+
+mixin template Constructor() {
+  import std.traits : FieldNameTuple;
+  this(Args...)(Args args) {
+    // static foreach(i, v; args) {
+    foreach(i, v; args) {
+      mixin("this." ~ FieldNameTuple!(typeof(this))[i]) = v;
+    }
+  }
+}
+
+void scanln(Args...)(auto ref Args args) {
+  enum sep = " ";
+  enum n = Args.length;
+  enum fmt = n.rep!(()=>"%s").join(sep);
+
+  string line = readln.chomp;
+  static if (__VERSION__ >= 2074) {
+    line.formattedRead!fmt(args);
+  } else {
+    enum argsTemp = n.iota.map!(
+      i => "&args[%d]".format(i)
+    ).join(", ");
+    mixin(
+      "line.formattedRead(fmt, " ~ argsTemp ~ ");"
+    );
+  }
+}
+
+// fold was added in D 2.071.0
+static if (__VERSION__ < 2071) {
+  template fold(fun...) if (fun.length >= 1) {
+    auto fold(R, S...)(R r, S seed) {
+      static if (S.length < 2) {
+        return reduce!fun(seed, r);
+      } else {
+        return reduce!fun(tuple(seed), r);
+      }
+    }
+  }
+}
+
+// popcnt with ulongs was added in D 2.071.0
+static if (__VERSION__ < 2071) {
+  ulong popcnt(ulong x) {
+    x = (x & 0x5555555555555555L) + (x>> 1 & 0x5555555555555555L);
+    x = (x & 0x3333333333333333L) + (x>> 2 & 0x3333333333333333L);
+    x = (x & 0x0f0f0f0f0f0f0f0fL) + (x>> 4 & 0x0f0f0f0f0f0f0f0fL);
+    x = (x & 0x00ff00ff00ff00ffL) + (x>> 8 & 0x00ff00ff00ff00ffL);
+    x = (x & 0x0000ffff0000ffffL) + (x>>16 & 0x0000ffff0000ffffL);
+    x = (x & 0x00000000ffffffffL) + (x>>32 & 0x00000000ffffffffL);
+    return x;
+  }
+}
