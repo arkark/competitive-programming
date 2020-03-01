b9847b7c1b134026ee80ead5fa86bad0a17cfcf9
Takeshi Kaneko <target.ark@gmail.com>
2020-03-02 00:19:59 +0900

    Update compro.sh

--- compro.sh
+++ compro.sh
@@ -1,16 +1,30 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # ./compro.sh a b
-#  -> create dir/a.d, dir/b.d
+#  -> Create dir/a.d, dir/b.d
 
 folderName="dir"
 templateFileName="template.d"
 
 if [ -e $folderName ]; then
-    echo "folder '$folderName' already exists"
+    echo "Folder '$folderName' already exists"
 else
     mkdir $folderName
+
     for fileName in $@; do
         cp $templateFileName "$folderName/$fileName.d"
     done
+
+    printf "Input a contest URL: "
+    read URL
+    TASK_URL=$(echo $URL | sed -E 's/(^.*)\/([^\/]+)\/?$/\1\/\2\/tasks\/\2_/')
+
+    printf "%s\n" \
+        'command = "rdmd {}.d"'\
+        'file_name = "{}.d"' \
+        "task_url = \"$TASK_URL{}\"" \
+        > "$folderName/.config.toml"
+
+    echo "Created:"
+    find $folderName -not -type d
 fi
