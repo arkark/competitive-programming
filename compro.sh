1ef541d19e344eff66cebb80805693857a546a11
Takeshi Kaneko <target.ark@gmail.com>
2020-03-20 23:51:22 +0900

    Update compro.sh

--- compro.sh
+++ compro.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # ./compro.sh a b
 #  -> Create workspace/a.d, workspace/b.d
@@ -6,25 +6,27 @@
 folderName="workspace"
 templateFileName="template.d"
 
-if [ -e $folderName ]; then
-    echo "Folder '$folderName' already exists"
-else
-    mkdir $folderName
+pushd $(git rev-parse --show-toplevel)
+    if [ -e $folderName ]; then
+        echo "Folder '$folderName' already exists"
+    else
+        mkdir $folderName
 
-    for fileName in $@; do
-        cp $templateFileName "$folderName/$fileName.d"
-    done
+        for fileName in $@; do
+            cp $templateFileName "$folderName/$fileName.d"
+        done
 
-    printf "Input a contest URL: "
-    read URL
-    TASK_URL=$(echo $URL | sed -E 's/(^.*)\/([^\/]+)\/?$/\1\/\2\/tasks\/\2_/')
+        printf "Input a contest URL: "
+        read URL
+        TASK_URL=$(echo $URL | sed -E 's/(^.*)\/([^\/]+)\/?$/\1\/\2\/tasks\/\2_/')
 
-    printf "%s\n" \
-        'command = "rdmd {}.d"'\
-        'file_name = "{}.d"' \
-        "task_url = \"$TASK_URL{}\"" \
-        > "$folderName/.config.toml"
+        printf "%s\n" \
+            'command = "rdmd {}.d"'\
+            'file_name = "{}.d"' \
+            "task_url = \"$TASK_URL{}\"" \
+            > "$folderName/.config.toml"
 
-    echo "Created:"
-    find $folderName -not -type d
-fi
+        echo "Created:"
+        find $folderName -not -type d
+    fi
+popd
